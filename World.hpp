e2946eaa6f33cd9fda861d009aa243024240947b
Charlie Birks <charlie@daft.games>
2024-03-26 19:56:09 +0000

    Move Object to its own file

--- World.hpp
+++ World.hpp
@@ -7,75 +7,9 @@
 #include <string>
 #include <vector>
 
+#include "Object.hpp"
 #include "ObjectDataStore.hpp"
 #include "TextureLoader.hpp"
-
-struct Minifig
-{
-    uint32_t id;
-    std::string name;
-};
-
-class Object
-{
-public:
-    Object(uint16_t id, uint16_t x, uint16_t y, std::string name, std::shared_ptr<SDL_Texture> texture, const ObjectData *data);
-
-    void update(uint32_t deltaMs);
-
-    void render(SDL_Renderer *renderer, int scrollX, int scrollY, int z, float zoom);
-    void renderDebug(SDL_Renderer *renderer, int scrollX, int scrollY, float zoom);
-
-    uint16_t getId() const;
-
-    int getX() const;
-    int getY() const;
-
-    void setX(int x);
-    void setY(int y);
-    void setPosition(int x, int y);
-
-    const ObjectData *getData() const;
-
-    void replace(uint16_t newId, std::shared_ptr<SDL_Texture> newTex = nullptr, const ObjectData *newData = nullptr);
-
-    void addMinifig(Minifig &&minifig);
-
-    const ObjectData::Frameset *getCurrentFrameset() const;
-    int getFrameDelay() const;
-
-    void setDefaultAnimation();
-    void setAnimation(int index);
-
-    std::tuple<int, int> getFrameSize() const;
-
-    void setPixelPos(float x, float y);
-
-    void setTargetPos(int tx, int ty, int vx, int vy, bool reverse = false);
-
-private:
-    uint16_t id;
-    int x, y;
-    std::string name;
-
-    std::shared_ptr<SDL_Texture> texture;
-    const ObjectData *data;
-
-    std::vector<Minifig> minifigs;
-
-    int currentAnimation = -1, nextAnimation = -1;
-    int currentAnimationFrame = 0;
-    int animationTimer = 0;
-
-    // "screen" aligned objects
-    float pixelX = 0.0f, pixelY = 0.0f;
-
-    // moving objects
-    int targetX = 0, targetY = 0;
-    int velX = 0, velY = 0;
-
-    bool reverse = false;
-};
 
 class World final
 {
